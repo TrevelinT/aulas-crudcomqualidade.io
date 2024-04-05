342150a4efcbd54946fb2f09b5d6d7dcf6484c86
Danilo Trevelin <danilo.muller.trevelin@gmail.com>
2024-04-05 17:40:52 -0300

    A2.4

--- core/crud.ts
+++ core/crud.ts
@@ -5,8 +5,10 @@ const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
+type UUID = string;
+
 interface Todo {
-  id: string;
+  id: UUID;
   date: string;
   content: string;
   done: boolean;
@@ -42,7 +44,7 @@ function read(): Array<Todo> {
   return db.todos;
 }
 
-function update(id: string, partialTodo: Partial<Todo>): Todo {
+function update(id: UUID, partialTodo: Partial<Todo>): Todo {
   let updatedTodo;
   const todos = read();
   todos.forEach((currentTodo) => {
@@ -63,10 +65,25 @@ function update(id: string, partialTodo: Partial<Todo>): Todo {
   return updatedTodo;
 }
 
-function updateContentById(id: string, content: string): Todo {
+function updateContentById(id: UUID, content: string): Todo {
   return update(id, {
     content,
   });
+}
+
+function deleteById(id: UUID) {
+  const todos = read();
+
+  const todosWithoutOne = todos.filter((todo) => {
+    if(todo.id === id) {
+      return false;
+    }
+    return true;
+  });
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+    todos: todosWithoutOne,
+  }, null, 2));
 }
 
 function CLEAR_DB() {
@@ -76,11 +93,14 @@ function CLEAR_DB() {
 // [SIMULATION]
 CLEAR_DB();
 create("Primeira TODO");
-create("Primeira TODO");
-const terceiraTodo = create("Segunda TODO");
-// update(terceiraTodo.id, {
+const secondTodo = create("Primeira TODO");
+deleteById(secondTodo.id);
+const thirdTodo = create("Segunda TODO");
+// update(thirdTodo.id, {
 //   content: "Atualizada!",
 //   done: true,
 // });
-updateContentById(terceiraTodo.id,  "Atualizada!");
-console.log(read());
+updateContentById(thirdTodo.id,  "Atualizada!");
+const todos = read();
+console.log(todos);
+console.log(todos.length);
